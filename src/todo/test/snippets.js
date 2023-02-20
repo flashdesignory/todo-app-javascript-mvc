d6bbae9a56ce6d688c5e550222050970b07e1cc3
Thorsten Kober <tkober@gmail.com>
2023-02-20 15:14:46 -0800

    Create snippets.js

---
+++ src/todo/test/snippets.js
@@ -0,0 +1,16 @@
+export const editTodoWithClick = (element, value) => {
+    jest.useFakeTimers();
+    element.click();
+    jest.advanceTimersByTime(50);
+    element.click();
+    jest.advanceTimersByTime(50);
+    element.textContent = value;
+    element.blur();
+    jest.useRealTimers();
+}
+
+export const editTodoWithKeys = (element, value) => {
+    element.dispatchEvent(new KeyboardEvent("keyup", { key: " " }));
+    element.textContent = value;
+    element.dispatchEvent(new KeyboardEvent("keyup", { key: "Enter" }));
+}
