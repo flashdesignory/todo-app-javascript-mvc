c21df0e1d12ab76eb4ca6aaaf9df9852bbd4eedc
Thorsten Kober <tkober@gmail.com>
2023-08-08 12:48:56 -0700

    cleanup

--- speedo.js
+++ speedo.js
@@ -5,10 +5,16 @@ window.onmessage = async (event) => {
     // ensure we only let legit functions run...
     if (event.data.id !== appId || event.data.type !== "speedo") return;
 
+    const { name } = event.data;
+
     const testFunction = new Function('return ' + event.data.fn)();
     if (testFunction) {
-      await testFunction();
-      window.top.postMessage({ type: "test-completed", status: "success" }, "*");
+        performance.mark(`${name}-start`);
+        await testFunction();
+        performance.mark(`${name}-end`);
+        performance.measure(`${name}`, { start: `${name}-start`, end: `${name}-end` });
+        const result = JSON.stringify(performance.getEntriesByName(`${name}`)[0]);
+        window.top.postMessage({ type: "test-completed", status: "success", result }, "*");
     }
 }
 
