e03c91c9960ce3a31ef132a5fe819af977e9a124
Thorsten Kober <tkober@gmail.com>
2023-08-18 12:15:34 -0700

    speedo update

--- speedo.js
+++ speedo.js
@@ -3,19 +3,26 @@ const appId = "1234"
 
 window.onmessage = async (event) => {
     // ensure we only let legit functions run...
-    if (event.data.id !== appId || event.data.type !== "speedo") return;
+    if (event.data.id !== appId || event.data.type !== "speedo")
+        return;
 
     const { name } = event.data;
 
-    const testFunction = new Function('return ' + event.data.fn)();
+    const testFunction = new Function(`return ${ event.data.fn}`)();
     if (testFunction) {
-        performance.mark(`${name}-start`);
-        await testFunction();
-        performance.mark(`${name}-end`);
-        performance.measure(`${name}`, { start: `${name}-start`, end: `${name}-end` });
-        const result = JSON.stringify(performance.getEntriesByName(`${name}`)[0]);
-        window.top.postMessage({ type: "test-completed", status: "success", result }, "*");
+        requestAnimationFrame(() => {
+            performance.mark(`${name}-start`);
+        });
+        requestAnimationFrame(async () => {
+            await testFunction();
+            setTimeout(() => {
+                performance.mark(`${name}-end`);
+                performance.measure(`${name}`, { start: `${name}-start`, end: `${name}-end` });
+                const result = JSON.stringify(performance.getEntriesByName(`${name}`)[0]);
+                window.top.postMessage({ type: "test-completed", status: "success", result }, "*");
+            }, 0);
+        });
     }
-}
+};
 
 window.top.postMessage({ type: "app-ready", status: "success", appId }, "*");
